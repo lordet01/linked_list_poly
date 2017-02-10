58d64a6fce1e3798cab7152e4ee54e9f26348ec1
Kwang Myung, Jeon <lordet01@gmail.com>
2017-02-10 15:45:55 +0900

    reverted
    came back to v 0.1

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ int main(void)
 	//Initialize value
 	push(&pPoly_a, 1, 0);
 	push(&pPoly_a, 2, 8);
-	push(&pPoly_a, 3, 12);
+	push(&pPoly_a, 3, 14);
 
 	push(&pPoly_b, 10, 6);
 	push(&pPoly_b, -3, 10);
